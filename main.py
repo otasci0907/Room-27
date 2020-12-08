75a35647956ad627655dd1df75ce650f326a52c3
Orhan Tasci <orhant@Orhans-MBP.fios-router.home>
2020-12-08 14:48:20 -0500

    added score mechanism

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
+import constants as cn
 
 pygame.init()
+pygame.font.init()
 
-WIDTH = 1280
-HEIGHT = 720
+myfont = pygame.font.SysFont('Atari Classic', 40)
 
-win = pygame.display.set_mode((WIDTH, HEIGHT))
+win = pygame.display.set_mode((cn.WIDTH, cn.HEIGHT))
 pygame.display.set_caption("Room 27")
 
 clock = pygame.time.Clock()
@@ -19,16 +20,18 @@ class Player(object):
         self.vel = 10
         self.left = False
         self.right = False
+        self.score = 0
 
     def draw(self, win):
-        pygame.draw.rect(win, (255,0,0), (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(win, cn.RED, (self.x, self.y, self.width, self.height))
 
 def redrawGameWindow():
     win.fill((0,0,0))
     player.draw(win)
+    win.blit(textsurface,(10,10))
     pygame.display.update()
 
-player = Player(604, 300, 76, 120)
+player = Player(cn.WIDTH / 2, cn.HEIGHT / 2, 76, 120)
 run = True
 while run:
     clock.tick(27)
@@ -50,7 +53,13 @@ while run:
 
     if keys[pygame.K_DOWN] or keys[pygame.K_s]:
         player.y += player.vel
-            
+
+    if keys[pygame.K_x] or keys[pygame.K_ESCAPE]:
+        pygame.quit()
+
+    player.score = int(pygame.time.get_ticks() / 1000)
+    textsurface = myfont.render(str(player.score), False, (255, 255, 255))
+
     redrawGameWindow()
 
 pygame.quit()
