04d970c7f20372b46141272758aff2ba0ec9e5bc
otasci0907 <otasci0907@gmail.com>
2020-12-13 23:22:21 -0500

    created levels.py and added text & furniture class

--- levels.py
+++ levels.py
@@ -5,7 +5,34 @@ import pygame
 pygame.init()
 pygame.font.init()
 
-playText = cn.basicText.render("Play", False, (255, 255, 255))
+class Text(object):
+    #all the variables stored in the player object
+    def __init__(self, x, y, color, text, size):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.text = text
+        self.size = size
+        self.font = pygame.font.SysFont('Atari Classic', self.size)
+    
+    def draw(self, win):
+        renderText = self.font.render(self.text, False, self.color)
+        text_width = renderText.get_width()
+        text_height = renderText.get_height()
+        win.blit(renderText, (self.x - text_width / 2, self.y - text_height / 2))
+
+
+class Furniture(pygame.sprite.Sprite):
+    #all the variables stored in the player object
+    def __init__(self,x,y,width,height):
+        pygame.sprite.Sprite.__init__(self)
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+playText = Text(640, 360, cn.WHITE, "Play", 40)
+titleText = Text(640, 100, cn.WHITE, "Room 27", 150)
 
 def titleScreen():
     cn.bg = pygame.image.load(os.path.join(cn.BG_DIR, 'titleScreen.jpg'))
